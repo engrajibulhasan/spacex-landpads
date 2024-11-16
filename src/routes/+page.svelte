e7ae010ebfdcbcfa6e08c757b759a30170f7786d
engrajibulhasan <engrajibulhasan@gmail.com>
2024-11-16 11:19:46 +0600

    Initial Commit wit file structure

---
+++ src/routes/+page.svelte
@@ -0,0 +1,7 @@
+<script>
+	import { Button } from 'flowbite-svelte';
+</script>
+
+<h1 class="text-red-600">Welcome to SvelteKit</h1>
+<Button color="blue">Hellow World</Button>
+<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
